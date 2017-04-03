c6d1b175281741c13bdbb745bf905f3aa4aad3e8
André Alves <Andr Alves@DESKTOP-AV2AMJ9>
2017-04-03 18:42:50 +0100

    pagina novo jog

---
+++ src/BlackJack/Models/Player.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.ComponentModel.DataAnnotations;
+
+namespace BlackJack.Models
+{
+    public class Player
+    {
+        [Required(ErrorMessage = "Por favor introduza o seu nome")]
+        public string Nome { get; set; }
+
+        [Required(ErrorMessage = "Por favor introduza a sua idade")]  //datanotations
+        public int Idade { get; set; }
+
+        public Player()
+        { }
+    }
+}
