26fc7c9b3072f80833f31ee4c397f500a330a8d0
André Alves <Andr Alves@DESKTOP-AV2AMJ9>
2017-04-03 21:59:17 +0100

    formulário feito

--- src/BlackJack/Models/Player.cs
+++ src/BlackJack/Models/Player.cs
@@ -8,10 +8,11 @@ namespace BlackJack.Models
 {
     public class Player
     {
-        [Required(ErrorMessage = "Por favor introduza o seu nome")]
+        [Required(ErrorMessage = "Por favor introduza o seu nome")]        
         public string Nome { get; set; }
 
-        [Required(ErrorMessage = "Por favor introduza a sua idade")]  //datanotations
+        [Required(ErrorMessage = "Por favor introduza a sua idade")]
+        [Range(18, 65, ErrorMessage = "Deve ser maior de Idade para jogar este jogo.") ]
         public int Idade { get; set; }
 
         public Player()
