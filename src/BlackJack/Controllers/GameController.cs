e0102793d957d61e7fef881f1068c17685b708e9
André Alves <Andr Alves@DESKTOP-TTCPO0M>
2017-05-09 22:30:26 +0100

    playeraction type a dar erro

--- src/BlackJack/Controllers/GameController.cs
+++ src/BlackJack/Controllers/GameController.cs
@@ -37,7 +37,7 @@ namespace BlackJack.Controllers
         }
 
         [HttpPost]
-        public IActionResult PlayGame(int id, int playerAction, double initialBet)
+        public IActionResult PlayGame(int id, PlayerAction playerAction, double initialBet)
         {
             if (ModelState.IsValid)
             {
