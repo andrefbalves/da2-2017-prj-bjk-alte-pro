9c438331df2fb48a25519b9b78bc3a625a908459
André Alves <Andr Alves@DESKTOP-TTCPO0M>
2017-05-16 23:22:51 +0100

    Pagina equipa e regras restruturadas com areas escondidas

--- src/BlackJack/wwwroot/js/site.js
+++ src/BlackJack/wwwroot/js/site.js
@@ -1,1 +1,58 @@
-﻿// Write your Javascript code.
+﻿$(document).ready(function () {
+
+    $(".btnrules").hide();
+
+    $("#btnhit").click(function () {
+        $("#hit").toggle();
+        $("#stand").hide();
+        $("#double").hide();
+        $("#surrender").hide();
+    });
+
+    $("#btnstand").click(function () {
+        $("#stand").toggle();
+        $("#hit").hide();
+        $("#double").hide();
+        $("#surrender").hide();
+    });
+
+    $("#btndouble").click(function () {
+        $("#double").toggle();
+        $("#hit").hide();
+        $("#stand").hide();
+        $("#surrender").hide();
+    });
+
+    $("#btnsurrender").click(function () {
+        $("#surrender").toggle();
+        $("#hit").hide();
+        $("#stand").hide();
+        $("#double").hide();
+    });
+});
+
+$('.roundbtn').click(function () {
+    $(this).toggleClass('active')
+    $(this).siblings().removeClass('active');
+});
+
+$(document).ready(function () {
+
+    $("#projeto").hide();
+
+    $("#btnequipa").click(function () {
+        $("#equipa").toggle();
+        $("#projeto").hide();
+    });
+
+    $("#btnmembros").click(function () {
+        $("#membros").toggle();
+        $("#projeto").hide();
+    });
+
+    $("#btnprojeto").click(function () {
+        $("#projeto").toggle();
+        $("#equipa").hide();
+        $("#membros").hide();
+    });
+});
