62996d069dd265f10eadc557ef1b9bb7a79cb8f0
Diogo <Diogo@DESKTOP-PD9ACG5>
2017-06-01 17:49:54 +0100

    modo autonomo a começar

---
+++ src/BlackJack/Controllers/AutonomoController.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using BlackJack.Models;
+using System.Net.Http;
+
+// For more information on enabling MVC for empty projects, visit http://go.microsoft.com/fwlink/?LinkID=397860
+
+namespace BlackJack.Controllers
+{
+    public class AutonomoController : Controller
+    {
+        [HttpGet]
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public IActionResult Index(NewGameApiRequest novoJogador)
+        {
+            if (ModelState.IsValid)
+            {
+                HttpClient client = MyHTTPClientNewGame.Client;
+                string path = "/api/NewGame";
+                HttpResponseMessage response = client.PostAsJsonAsync(path, novoJogador).Result;
+                if (!response.IsSuccessStatusCode)
+                {
+                    return View("Index");
+                }
+
+                PlayApiResponse ng = response.Content.ReadAsAsync<PlayApiResponse>().Result;
+                
+            }
+            
+        }
+    }
+}
