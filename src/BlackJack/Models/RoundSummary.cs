e21b8be9f73d7d318bf0d8943a20d8298ad25bab
Diogo <Diogo@DESKTOP-PD9ACG5>
2017-06-01 18:00:38 +0100

    modo auto

---
+++ src/BlackJack/Models/RoundSummary.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace BlackJack.Models
+{
+    public enum RoundResult { Win, Lose, Tie }
+
+    public class RoundSummary
+    {
+        public int Rounds { get; set; }
+
+        public double InitialCredits { get; set; }
+
+        public int Bet { get; set; }
+
+        public int RoundResult { get; set; }
+
+        public bool Double { get; set; }
+
+        public bool Blackjack { get; set; }
+
+        public bool DealerBlackjack { get; set; }
+
+        public double FinalCredits { get; set; }
+
+        public RoundSummary()
+        { }
+    }
+}
