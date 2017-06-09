227dde7dc7ec4046c4f146f3c02f04867d4cf650
André Alves <Andr Alves@DESKTOP-TTCPO0M>
2017-06-09 17:52:42 +0100

    bd inic

---
+++ src/BlackJack/Models/BlackJackDbContext.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace BlackJack.Models
+{
+    public class BlackJackDbContext : DbContext
+    {
+        public DbSet<BlackJack.Models.Game> Games { get; set; }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            string connection =
+            @"Server=(localdb)\mssqllocaldb;Database=DA2_2017_LABCOMPRAS;
+            Trusted_Connection=True;";
+            optionsBuilder.UseSqlServer(connection);
+        }
+    }
+}
